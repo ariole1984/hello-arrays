d2f5d29d9180b9814e0bfebc729ac4535a314f4a
Ariole Dieujuste <arioledieujuste@gmail.com>
2022-07-14 12:27:43 -0400

    Just a folder

---
+++ array.js
@@ -0,0 +1,46 @@
+console.log("Hello World");
+
+/*Create an array with a list of fruits starting with 3 fruit
+
+with that array now do the following
+- Add a new fruit to the end of the array
+- Display the array
+- Add a new fruit to the beginning of the array
+- Display the array
+- Remove the last fruit.
+- Display the array*/
+
+// LAB ASSIGNMENT
+
+
+// let fruits = ['kiwi','mango','apple']
+// fruits.push('avocado');
+// // console.log(fruits);
+// console.log(...fruits);
+
+// console.log('This is reg' + fruits)
+// console.log('This is reg, ', fruits)
+// console.log("This is spread", ...fruits)
+
+
+// let fruits = ['kiwi','mango','apple','peach']
+// fruits.unshift('peach');
+// console.log(unshift);
+
+const chickenEgg ['🥚']
+console.log(`The ${chickenEgg[0]}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
